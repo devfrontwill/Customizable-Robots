948713fddcd0a473bd5faaa86cbb896ab99e5f05
Wiliam Roza <devfrontwill@gmail.com>
2022-08-27 19:42:41 -0300

    Criado objetos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle]")
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector(".controle-contador") //
+    const peca = controle.querySelector("[data-contador]") //
 
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1
@@ -17,6 +17,4 @@ function manipulaDados(operacao, controle) {
     }
 }
 
-
-  
 
